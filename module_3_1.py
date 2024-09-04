571ce15ee2f62b32e4a7ca75a1dba90005c89432
user <kamillochka@inbox.ru>
2024-09-04 20:20:39 +0300

    переделала

--- module_3_1.py
+++ module_3_1.py
@@ -14,19 +14,12 @@ def is_contains(string, list_to_search):
     for item in list_to_search:
         if string == item.lower():
             return True
+    else:
             return False
 
 # Пример вызова функций
-result1 = string_info("Capybara")
-print(result1)
-
-result2 = string_info("Armageddon")
-print(result2)
-
-result3 = is_contains('Urban', ['ban', 'BaNaN', 'urBAN']) # Urban ~ urBAN)
-print(result3)
-
-result4 = is_contains('cycle', ['recycling', 'cyclic']) # No matches
-print(result4)
-
+print(string_info('Capybara'))
+print(string_info('Armageddon'))
+print(is_contains('Urban', ['ban', 'BaNaN', 'urBAN'])) # Urban ~ urBAN
+print(is_contains('cycle', ['recycling', 'cyclic'])) # No matches
 print(calls)
