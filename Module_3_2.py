4220d81bbc74a62e3478a00cd44fd76558038bbc
user <kamillochka@inbox.ru>
2024-09-12 14:34:38 +0300

    переделала false

---
+++ Module_3_2.py
@@ -0,0 +1,6 @@
+def send_email(message,recipient,*sender = "univerity.help@gmail.com"):
+    if ("@", ".com", ".ru", ".net") non in send_email()
+       print("Невозможно отправить письмо с адреса <sender> на адрес <recipient>")
+
+
+send_email(1,2,3)
