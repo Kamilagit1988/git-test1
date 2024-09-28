ff932cc20e865d6c1eea5792522f4a573e9974e1
user <kamillochka@inbox.ru>
2024-09-29 00:51:52 +0300

    переделала false

---
+++ module_4_1.py
@@ -0,0 +1,13 @@
+import fake_math as make
+import true_math as pake
+
+
+result1 = make(69, 3)
+result2 = make(3, 0)
+result3 = pake(49, 7)
+result4 = pake(15, 0)
+
+print(result1)
+print(result2)
+print(result3)
+print(result4)
