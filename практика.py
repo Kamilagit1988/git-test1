889bf85fc1574994de9ac34d540b974b596ce5e3
user <kamillochka@inbox.ru>
2024-12-21 14:49:12 +0300

    changes

--- практика.py
+++ практика.py
@@ -9,20 +9,46 @@ bot = Bot(token = api)
 dp = Dispatcher(bot, storage = MemoryStorage())
 
 class UserState(StatesGroup):
-    adress = State()
+    age = State()
+    growth = State()
+    weight = State()
 
-@dp.message_handler(text = "Заказать")
-async  def buy(message):
-    await message.answer("Отправь нам свой адрес, пожалуйста.")
-    await UserState.adress.set()
 
-@dp.message_handler(state = UserState.adress)
-async  def fsm_handler(message, state):
-   await state.update_data(first = message.text)
+@dp.message_handler(commands=['start'])
+async def start_message(message):
+
+    await message.answer("Привет! Я бот помогающий твоему здоровью.")
+@dp.message_handler(text = 'Calories')
+async def set_age(message):
+    await message.answer("Введите свой возраст.")
+    await UserState.age.set()
+
+@dp.message_handler(state = UserState.age)
+async def set_growth(message, state):
+   await state.update_data(age = message.text)
    data = await state.get_data()
-   await message.answer(f"Доставка будет отправлена на {data ['first']}")
-   await state.finish()
+   await message.answer(f"Введите свой рост.")
+   await UserState.growth.set()
 
+@dp.message_handler(state = UserState.growth)
+async def set_weight(message, state):
+   await state.update_data(growth = message.text)
+   data = await state.get_data()
+   await message.answer(f"Введите свой вес.")
+   await UserState.weight.set()
+@dp.message_handler(state=UserState.weight)
+async def send_calories(message, state):
+    await state.update_data(weight=message.text)
+    data = await state.get_data()
+    result = (10*int(data['weight'])+6.25*int(data['growth'])-5*int(data['age'])+5)
+    await message.answer(f'Ваша норма калорий: {result} ккал в сутки (для мужчин)')
+    await UserState.weight.set()
+    await state.finish()
+
+@dp.message_handler()
+async def all_message(message):
+    await message.answer("Введите команду /start чтобы начать общение.")
+    
 
 if __name__ == "__main__":
     executor.start_polling(dp, skip_updates = True)
